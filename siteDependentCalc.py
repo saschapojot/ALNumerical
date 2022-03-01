54210fd396a2dcf95b5cf0b40f2c27b2b2622d01
saschapojot <1992xi.liu@gmail.com>
2022-03-01 08:39:55 +0800

    site dependent

--- siteDependentCalc.py
+++ siteDependentCalc.py
@@ -1,9 +1,7 @@
-from twoSublatticesFuncs import *
+from siteDependentFuncs import *
 
-
-initVecMp=[y(n,0) for n in nRange]
-
-initVec=[complex(elem) for elem in initVecMp]
+initVecmp=[y(n,0) for n in nRange]
+initVec=[complex(elem) for elem in initVecmp]
 
 ########evolution
 tCalcStart=datetime.now()
@@ -15,9 +13,11 @@ for q in range(0,Q):
 tCalcEnd=datetime.now()
 print("calc time: ",tCalcEnd-tCalcStart)
 
-outDir="./twoHalves/s"+str(s)+"Gmax"+str(Gmax)+"tTot"+str(tTot)+"/"
+outDir="./siteDependent/verifys"+str(s)+"Gmax"+str(Gmax)+"tTot"+str(tTot)+"/"
 pltOut=outDir+"out/"
 tValsAll=[q*dt for q in range(0,Q+1)]
+
+
 ######plotting
 
 Path(pltOut).mkdir(parents=True,exist_ok=True)
@@ -30,7 +30,7 @@ for q in range(0,Q+1):
         continue
     plt.figure()
     psiCurr=psiAll[q]
-    absTmp=[mpmath.fabs(elem) for elem in psiCurr]
+    absTmp=[np.abs(elem) for elem in psiCurr]
     plt.plot(nRange,absTmp,color="black")
     plt.title("t="+str(round(dt*q,3)))
     plt.xlabel("site")
@@ -47,25 +47,32 @@ tPosStart=datetime.now()
 positionAll=[]
 for psiTmp in psiAll:
     positionAll.append(avgPos(psiTmp))
+
 #drift
 drift=[elem-positionAll[0] for elem in positionAll]
 #<x^{2}>
 position2All=[]
 for psiTmp in psiAll:
     position2All.append(avgPos2(psiTmp))
+
+
 #spread
 sigmaAll=[]
 for n in range(0,len(psiAll)):
-    sgmTmp=mpmath.sqrt(mpmath.fabs(position2All[n]-positionAll[n]**2))
+    sgmTmp=np.sqrt(np.abs(position2All[n]-positionAll[n]**2))
     sigmaAll.append(sgmTmp)
+
+
 #height
 heigtsAll=[]
 for psiTmp in psiAll:
     heigtsAll.append(height(psiTmp))
+
 #norm
 normsAll=[]
 for psiTmp in psiAll:
     normsAll.append(norm2(psiTmp))
+
 
 #plot drift
 
@@ -77,6 +84,7 @@ plt.ylabel("drift")
 plt.title("Gmax="+str(Gmax)+", tTot="+str(tTot)+", drift="+str(round(drift[-1],3)))
 plt.savefig(outDir+"Gmax="+str(Gmax)+", tTot="+str(tTot)+".png")
 plt.close()
+
 #plot sigma
 plt.figure()
 plt.plot(tValsAll,sigmaAll,color="black")
@@ -101,25 +109,7 @@ plt.xlabel("time")
 plt.ylabel("norm")
 plt.title("Gmax="+str(Gmax)+", tTot="+str(tTot))
 plt.savefig(outDir+"Gmax="+str(Gmax)+", tTot="+str(tTot)+"norm.png")
-plt.close()
 tPosEnd=datetime.now()
+
 print("calc pos time: ",tPosEnd-tPosStart)
-#####calc diff
-# tDiffStart=datetime.now()
-# trueVecsAll=[]
-# for q in range(0,Q+1):
-#     tq=dt*q
-#     yqMp=[y(n,tq) for n in nRange]
-#     yq=[complex(elem) for elem in yqMp]
-#     trueVecsAll.append(yq)
-#
-# dist=[]
-# for q in range(0,Q+1):
-#     diffTmp=np.array(psiAll[q])-np.array(trueVecsAll[q])
-#     dist.append(np.linalg.norm(diffTmp,ord=2))
-# tDiffEnd=datetime.now()
-# print("diff time: ",tDiffEnd-tDiffStart)
-# plt.figure()
-# plt.plot(range(0,Q+1),dist,color="black")
-# plt.savefig("tmp.png")
-# plt.close()
+plt.close()
---
+++ siteDependentFuncs.py
@@ -0,0 +1,256 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from datetime import datetime
+from pathlib import Path
+import mpmath
+from mpmath import mp
+
+mp.dps = 100
+
+# this script calculates evolution of wavefunction with site-dependent coefficients
+# calculates pumping for real t1 != t2
+k0 = 0.13
+k1 = 0.4 + 1.8j
+s = 11
+
+Gmax = 0.1
+Gamma = 0
+
+phi11s = np.log(1 / (
+        4 * (np.sinh(1 / 2 * (k1 + np.conj(k1)))) ** 2
+))
+tHalf = 10
+tTot = 2 * tHalf
+Q = 5000
+dt = tTot / Q
+print("dt=" + str(dt))
+nStart = -2000
+L = 2 * 2000  # lattice length
+nRange = range(nStart, nStart + L)
+
+
+def f(t):
+    return 0
+
+
+def g(t):
+    return 0
+
+
+def minusFunc(t):
+    return s - Gmax / tHalf * t
+
+
+def plusFunction(t):
+    return s + Gmax / tHalf * t
+
+
+def A1(t):
+    if t < tHalf:
+        return minusFunc(t)
+    else:
+        return s - Gmax
+
+
+def A2(t):
+    if t < tHalf:
+        return plusFunction(t)
+    else:
+        return s + Gmax
+
+
+def B1(t):
+    if t < tHalf:
+        return plusFunction(t)
+    else:
+        return s + Gmax
+
+
+def B2(t):
+    if t < tHalf:
+        return minusFunc(t)
+    else:
+        return s - Gmax
+
+
+def m(t):
+    return 1j * Gamma * t + 1j * np.exp(-1j * k0 - k1) * s * t + 1j * np.exp(1j * k0 + k1) * s * t
+
+
+def Cn(n, t):
+    if n % 2 == 0:
+        return A1(t)
+    else:
+        return B1(t)
+
+
+def Dn(n, t):
+    if n % 2 == 0:
+        return A2(t)
+    else:
+        return B2(t)
+
+
+def En(n, t):
+    if n % 2 == 0:
+        return Gamma + f(t)
+    else:
+        return Gamma + g(t)
+
+
+def y(n, t):
+    """
+
+    :param n:
+    :param t:
+    :return: exact 1-bright-soliton solution
+    """
+    return mpmath.exp(k1 * n + m(t)) / (1 +
+                                        mpmath.exp((k1 + np.conj(k1)) * n + m(t) + mpmath.conj(m(t)) + phi11s
+                                                   )
+                                        ) * mpmath.exp(1j * k0 * n + 1j * Gamma * t)
+
+
+####calc position expectation
+def avgPos(psiVec):
+    """
+
+    :param psiVec:
+    :return: <x>
+    """
+    rst = 0
+    for j in range(0, len(nRange)):
+        rst += nRange[j] * np.abs(psiVec[j]) ** 2
+
+    rst /= (np.linalg.norm(psiVec, 2)) ** 2
+    return rst
+
+
+def avgPos2(psiVec):
+    """
+
+    :param psiVec:
+    :return: <x^{2}>
+    """
+    rst = 0
+    for j in range(0, len(nRange)):
+        rst += (nRange[j]) ** 2 * np.abs(psiVec[j]) ** 2
+    rst /= (np.linalg.norm(psiVec, 2)) ** 2
+    return rst
+
+
+def norm2(psiVec):
+    """
+
+    :param psiVec:
+    :return: 2 norm pf psi
+    """
+    return np.linalg.norm(psiVec, 2)
+
+
+def height(psiVec):
+    """
+
+    :param psiVec:
+    :return: maximum height of psi
+    """
+    # absTmp=[np.abs(elem) for elem in psiVec]
+    return np.max(np.abs(psiVec))
+
+
+# RK4 part
+
+def K0Vec(psiVec, q):
+    """
+
+    :param psiVec:
+    :param q:
+    :return:
+    """
+    tq = dt * q
+    retK0 = np.zeros(L, dtype=complex)
+    for n in range(0, L):
+        retK0[n] = dt * 1j * (Cn(n, tq) * psiVec[(n - 1) % L] + Dn(n, tq) * psiVec[(n + 1) % L]) * (
+                    1 + np.abs(psiVec[n]) ** 2) \
+                   + dt * 1j * En(n, tq) * psiVec[n]
+
+    return retK0
+
+
+def K1Vec(psiVec, q, K0):
+    """
+
+    :param psiVec:
+    :param q:
+    :param K0:
+    :return:
+    """
+    tq = q * dt
+
+    retK1 = np.zeros(L, dtype=complex)
+    for n in range(0, L):
+        retK1[n] = dt * 1j * (Cn(n, tq + 1 / 2 * dt) * (psiVec[(n - 1) % L] + 1 / 2 * K0[(n - 1) % L])
+                              + Dn(n, tq + 1 / 2 * dt) * (psiVec[(n + 1) % L] + 1 / 2 * K0[(n + 1) % L])
+                              ) * (1 + np.abs(psiVec[n] + 1 / 2 * K0[n]) ** 2) \
+                   + dt * 1j * En(n, tq + 1 / 2 * dt) * (psiVec[n] + 1 / 2 * K0[n])
+
+    return retK1
+
+
+def K2Vec(psiVec, q, K1):
+    """
+
+    :param psiVec:
+    :param q:
+    :param K1:
+    :return:
+    """
+    tq = q * dt
+    retK2 = np.zeros(L, dtype=complex)
+
+    for n in range(0, L):
+        retK2[n] = dt * 1j * (
+                Cn(n, tq + 1 / 2 * dt) * (psiVec[(n - 1) % L] + 1 / 2 * K1[(n - 1) % L]) \
+                + Dn(n, tq + 1 / 2 * dt) * (psiVec[(n + 1) % L] + 1 / 2 * K1[(n + 1) % L])
+        ) * (1 + np.abs(psiVec[n] + 1 / 2 * K1[n]) ** 2) \
+                   + dt * 1j * En(n, tq + 1 / 2 * dt) * (psiVec[n] + 1 / 2 * K1[n])
+
+    return retK2
+
+
+def K3Vec(psiVec, q, K2):
+    """
+
+    :param psiVec:
+    :param q:
+    :param K2:
+    :return:
+    """
+    tq = dt * q
+    retK3 = np.zeros(L, dtype=complex)
+    for n in range(0, L):
+        retK3[n] = dt * 1j * (
+                Cn(n, tq + dt) * (psiVec[(n - 1) % L] + K2[(n - 1) % L]) \
+                + Dn(n, tq + dt) * (psiVec[(n + 1) % L] + K2[(n + 1) % L])
+        ) * (1 + np.abs(psiVec[n] + K2[n]) ** 2) \
+                   + dt * 1j * En(n, tq + dt) * (psiVec[n] + K2[n])
+
+    return retK3
+
+
+def oneStepRK4(psiVec, q):
+    """
+
+    :param psiVec:
+    :param q:
+    :return:
+    """
+    K0 = K0Vec(psiVec, q)
+    K1 = K1Vec(psiVec, q, K0)
+    K2 = K2Vec(psiVec, q, K1)
+    K3 = K3Vec(psiVec, q, K2)
+    psiNext = []
+    for n in range(0, L):
+        psiNext.append(psiVec[n] + 1 / 6 * (K0[n] + 2 * K1[n] + 2 * K2[n] + K3[n]))
+
+    return psiNext
